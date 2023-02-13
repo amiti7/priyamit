6dbf6a18e0b2ad1c7c5a40494e4abac736000f57
Amit Verma <cs.amit.v@gmail.com>
2023-02-13 11:57:15 +0530

    update css

---
+++ src/components/LocationData.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import maps from "../Assets/maps.png";
+
+const LocationData = () => {
+  return (
+    <div style={{justifyContent:'center', display:'grid'}} >
+    <div className="mb-2">
+      <div className="mr-1">
+        <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" style={{color:'#83181b'}}>Venue:UDAY MARRIAGE LAWN, Sathigawan.</div>
+      </div>
+    </div>
+    <br></br>
+    <div style={{color:'#83181b'}} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">To reach the venue, Click the below google map location : </div>
+
+    <div style={{right:"35%"}}>
+    <div style={{margin:'2em auto', width:'20%', border:'1px solid green', borderRadius:'15px', padding:'1em'}} onClick={()=>{window.parent.open("https://goo.gl/maps/h4hqtC6K3sux45We9","_new")}}>
+                <img width="50px" src={maps} alt=""></img>
+            </div>    </div>
+  </div>
+  )
+}
+
+export default LocationData
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import two from "../Assets/two.jpeg";
-import maps from "../Assets/maps.png";
+import LocationData from './LocationData';
 
 const Locations = () => {
-    return (
-        <div style={{ position:'absolute', width:'100%'}}>
-          <div style={{border:'1px solid #83181b', height:'90vh',overflow:'hidden', position:'absolute',top:'40%', borderRadius:'15px'}}>
-            <img height="100%" width="100%" src={two} alt="">
-            </img>
-        </div>
-         <div style={{position:'relative', marginTop:'10%', zIndex:2}}>
-            <div style={{position:'relative', fontSize:'16px', width:'60%',  textAlign:'center',color:'#83181b', transform:'translateY(600%)',margin:'auto', textTransform:'capitalize'}}> 
-            Venue:UDAY MARRIAGE LAWN, Sathigawan.
-            
-           
-            </div>
-            <div style={{position:'relative', fontSize:'16px', width:'80%',  textAlign:'center',color:'#83181b', transform:'translateY(650%)',margin:'auto', textTransform:'capitalize'}}> 
-            To reach the venue, Click the below google map location : 
-           
-            </div>
-            <div style={{margin:'auto', width:'20%', transform:'translateY(300%)', border:'1px solid green', borderRadius:'15px', padding:'1em'}} onClick={()=>{window.parent.open("https://goo.gl/maps/h4hqtC6K3sux45We9","_new")}}>
-                <img width="50px" src={maps} alt=""></img>
-            </div>
-           
-        </div>
-        </div>
-      )
+  return (
+    <div style={{ position:'absolute', width:'100%'}}>
+    <div style={{border:'1px solid #83181b', height:'90vh',overflow:'hidden', position:'absolute',top:'13%', borderRadius:'15px'}}>
+      <img height="100%" width="100%" src={two} alt="">
+      </img>
+  </div>
+   <div style={{position:'relative', marginTop:'80%'}}>
+    <LocationData/>
+  </div>
+  </div>
+  )
 }
 
 export default Locations
